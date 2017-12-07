be0ea4b13f660989cd61ee24e70eadac98b1420c
DESKTOP-JS8DOEJ\jiharu <kukaro4515@gmail.com>
2017-12-08 05:40:25 +0900

    v0.1.4 Add key logic

--- Aurora/Form1.cs
+++ Aurora/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Utilities;
 
 namespace Aurora
 {
     public partial class Form1 : Form
     {
+        globalKeyboardHook gkh = new globalKeyboardHook();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gkh.HookedKeys.Add(Keys.A);
+            gkh.HookedKeys.Add(Keys.B);
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+        }
+
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Up\t" + e.KeyCode.ToString());
+            e.Handled = true;
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Down\t" + e.KeyCode.ToString());
+            e.Handled = true;
         }
     }
 }
