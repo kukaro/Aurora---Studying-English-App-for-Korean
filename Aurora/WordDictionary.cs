f65d12446c63faeb3cd999cbe1da70961f4cacf5
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-10 18:32:09 +0900

    v0.2.2 Add Data namespace

---
+++ Aurora/WordDictionary.cs
@@ -0,0 +1,11 @@
+﻿using Dto;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Data{
+    public class WordDictionary
+    {
+        private Dictionary<string, Word> dict;
+
+    }
+}
