14502616b224bbc1ebc607e9c2dd791d629a380e
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-10 19:32:52 +0900

    v0.2.3 Add WordDict

--- Aurora/WordDictionary.cs
+++ Aurora/WordDictionary.cs
@@ -6,6 +6,14 @@ namespace Data{
     public class WordDictionary
     {
         private Dictionary<string, Word> dict;
+        public WordDictionary()
+        {
+            dict = new Dictionary<string, Word>();
+        }
 
+        public int add()
+        {
+            return dict.Count;
+        }
     }
 }
