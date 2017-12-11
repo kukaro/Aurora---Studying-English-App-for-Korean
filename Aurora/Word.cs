774bbbaf70094e04314dd86462f78008308ad7ef
DESKTOP-JS8DOEJ\jiharu <justkukaro@naver.com>
2017-12-12 02:57:33 +0900

    v0.2.5 Edit Mean

--- Aurora/Word.cs
+++ Aurora/Word.cs
@@ -1,10 +1,15 @@
-﻿namespace Dto
+﻿using dto;
+using System;
+using System.Collections;
+
+namespace Dto
 {
     class Word
     {
         private string word;
         private int appearanceFrequency;
         private int searchFrequency;
+        private ArrayList meanList;
 
         public int AppearanceFrequency
         {
@@ -21,6 +26,7 @@
             this.word = word;
             appearanceFrequency = 1;
             searchFrequency = 0;
+            meanList = new ArrayList();
         }
 
         public void increaseSearchfrequency()
@@ -31,6 +37,19 @@
         public void increaseAppearancefrequency()
         {
             appearanceFrequency++;
+        }
+
+        public bool addMean(Mean m)
+        {
+            try
+            {
+                meanList.Add(m);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return false;
         }
     }
 }
